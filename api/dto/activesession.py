0ea9f8ddebbe47caf0d38b9ed0e6c77ead095c76
Jeahy <jessika.weber@gmail.com>
2024-03-03 10:55:11 +0000

    Untrack 'api' directory

--- api/dto/activesession.py
+++
@@ -1,8 +0,0 @@
-from pydantic import BaseModel
-from datetime import datetime
-
-class ActiveSession(BaseModel):
-    id: int  # Add an ID field for the primary key
-    username: str
-    access_token: str
-    expiry_time: datetime
--- api/dto/user.py
+++
@@ -1,7 +0,0 @@
-#we define a data model for our database
-from pydantic import BaseModel
-
-class User(BaseModel):
-    id: int  # Add an ID field for the primary key
-    username: str
-    password: str
